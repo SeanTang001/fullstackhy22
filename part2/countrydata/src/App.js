5680012fe8f9d96789024a967558a577f925424b
Sean Tang <seantang001>
2022-09-24 00:32:42 -0700

    end of part2c

--- part2/countrydata/src/App.js
+++ part2/countrydata/src/App.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 const App = () => {
   const [countries, setCountries] = useState([])
-  const [countriesToShow, setCountriesToShow] = useState([])
+  const [countryToShow, setCountryToShow] = useState(0)
   const [country, setCountry] = useState('')
 
   useEffect( () => {
@@ -16,13 +16,14 @@ const App = () => {
   }, [])
 
   const searchCountry = (event) => {
-    console.log(event.target.value)
+    //console.log(event.target.value)
     setCountry(event.target.value)
+    setCountryToShow(1)
   }
 
   
   const tz = () => {
-    console.log(country)
+    //console.log(country)
     let temp = countries.filter(
       (countri) => {
         //console.log(countri.name.common.toLowerCase().includes(country.toLowerCase()))
@@ -31,33 +32,81 @@ const App = () => {
     )
     
     if (temp.length > 10) {
-      temp = ([{name:{common:"Too many matches, specify another filter"}}])
+      return (<p>Too many matches, specify another filter</p>)
     }
-    else if (temp.length == 0) {
-      temp = ([{name:{common:" "}}])
+    else if (temp.length == 1 || !countryToShow) {
+      return (<p></p>)
     }
 
-    console.log(temp.length)
+    //console.log(temp.length)
 
     return(temp.map(
-      contri => <CountryEntry id = {contri.name.common} name = {contri.name.common}/>
+      contri => {
+        return(
+        <>
+          <CountryEntry id = {contri.name.common} name = {contri.name.common}/>       
+          <button onClick={processCountry} id={contri.name.common} type='button'>show</button>
+        </>
+      )}
       ))
   }
 
+const processCountry = (event) => {
+  console.log(event.target.id)
+  setCountry(event.target.id)
+  setCountryToShow(0)
+}
+
+const individualCountry = () => {
+  let tempz = countries.filter(
+    (countri) => {
+      //console.log(countri.name.common.toLowerCase().includes(country.toLowerCase()))
+      return(countri.name.common.toLowerCase() === (country.toLowerCase()))
+    }
+  )
+  let corrCountry = tempz[0]
+
+  if (tempz.length == 1){
+    let langs = corrCountry.languages
+    let langz = []
+    for (var key in langs) {
+      langz.push(<p>{langs[key]}</p>)
+    }
+
+    console.log("langz")
+    console.log(langz)
+    return(
+      <>
+      <h2>{corrCountry.name.common}</h2>
+      <p>Capital {corrCountry.capital[0]}</p>
+      <p>Area {corrCountry.area}</p>
+      <br></br>
+      <h1>languages</h1>
+        {
+          langz
+        }
+      <img src = {corrCountry.flags.png}/>
+      </>
+    )
+  }
+}
   return (
     <div>
       find countries <input onChange = {searchCountry}/>
-      <ul>
+      <>
       {
         tz()
       }
-      </ul>
+      {
+        individualCountry()
+      }
+      </>
     </div>
   )
 }
 
 const CountryEntry = ({name}) => {
-  return(<li>{name}</li>)
+  return(<p>{name}</p>)
 }
 
 export default App;
